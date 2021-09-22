bb4b6d314d4cff10e67919b7bb7afa83aa8ef282
Alice-Retz <alicegretz@gmail.com>
2021-09-22 18:20:04 -0500

    All tests written and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const twilio = require('twilio');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -25,6 +26,43 @@ describe('03_separation-of-concerns-demo routes', () => {
           id: '1',
           quantity: 10,
         });
+      });
+  });
+
+  it('should return all orders', async () => {
+    const order = await Order.insert({ quantity: 10 });
+    return request(app)
+      .get('/api/v1/orders')
+      .then((res) => {
+        expect(res.body).toEqual([order]);
+      });
+  });
+
+  it('should return all orders', async () => {
+    const order = await Order.insert({ quantity: 10 });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual([order]);
+      });
+  });
+
+  it('should update an order by id', async () => {
+    const order = await Order.insert({ quantity: 10 });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .send({ quantity: 20 })
+      .then((res) => {
+        expect(res.body).toEqual({ id: 1, quantity: 20 });
+      });
+  });
+
+  it('should delete an order', async () => {
+    const order = await Order.insert({ quantity: 10 });
+    return request(app)
+      .delete(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).not.toEqual(order);
       });
   });
 });
